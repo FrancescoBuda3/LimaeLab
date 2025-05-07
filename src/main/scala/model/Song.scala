b1348f15c14c1d0099e2b5c26421e3e4c96836b4
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-05-07 18:48:44 +0200

    feat: implements Song

--- src/main/scala/model/Song.scala
+++ src/main/scala/model/Song.scala
@@ -11,3 +11,14 @@ object Song:
    */
   opaque type Song = SongImpl
   
+  def song(title: String, verses: Seq[String])(using hyphFunc: String => Seq[String]): Song =
+    SongImpl(verse(title), verses.map(verse(_)))
+    
+  extension (s: Song)
+    def title: Verse = s.title
+    def verses: Seq[Verse] = s.verses
+    def appendVerse(verse: Verse): Song = SongImpl(s.title, s.verses :+ verse)
+    def removeVerse(index: Int): Song = SongImpl(s.title, s.verses.take(index) ++ s.verses.drop(index + 1))
+    def replaceVerse(index: Int, verse: Verse): Song = SongImpl(s.title, s.verses.updated(index, verse))
+    def replaceTitle(title: Verse): Song = SongImpl(title, s.verses)
+  
--- src/main/scala/model/Verse.scala
+++ src/main/scala/model/Verse.scala
@@ -1,8 +1,8 @@
 package model
 
 import hyphenation.Hyphenator.*
-import model.Language.Italian
-import model.Verse.verse
+
+import scala.util.matching.Regex
   
 object Verse:
 
@@ -32,36 +32,6 @@ object Verse:
     def hyphPoints: Seq[Int] = v.hyphPoints
     def addHyphPoint(hyphPoint: Int): Verse = VerseImpl(v.text, v.hyphPoints.appended(hyphPoint).sorted)
     def removeHyphPoint(hyphPoint: Int): Verse = VerseImpl(v.text, v.hyphPoints.filter(_ != hyphPoint))
+      
+      
 
-//@main def main(): Unit = {
-//  import java.io.InputStream
-//  import org.apache.fop.hyphenation.{HyphenationTree, Hyphenation}
-//  import org.xml.sax.InputSource
-//  // 1. Carica il pattern di sillabazione per l'italiano
-//  val lang = "it"
-//  val country = "" // vuoto se non necessario
-//  val patterStream: InputStream =
-//    getClass.getResourceAsStream(s"/hyph/$lang${
-//      if (country.nonEmpty) "_" + country else ""
-//    }.xml")
-//
-//  require(patterStream != null, s"Pattern di sillabazione non trovato per $lang")
-//
-//  // 2. Costruisci l'albero di sillabazione
-//  val tree = new HyphenationTree
-//  tree.loadPatterns(InputSource(patterStream))
-//
-//  // 3. Sillaba la parola
-//  val word = "nel mezzo del cammin di nostra vita"
-//  // remainChars: minimo di caratteri all'inizio; pushChars: minimo alla fine
-//  val remainChars = 1
-//  val pushChars = 1
-//  val hyph: Hyphenation = tree.hyphenate(word, remainChars, pushChars)
-//
-//  // 4. Stampa il risultato
-//  if (hyph != null) {
-//    println(s"Punti di sillabazione per '$word': ${hyph.toString}")
-//  } else {
-//    println(s"Nessun punto di sillabazione trovato per '$word'")
-//  }
-//}
