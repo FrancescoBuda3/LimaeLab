4ac715d26430e42b5a9249a90f95c9bbd8ab1b33
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-08-05 17:17:07 +0200

    add test Line should have the length of the string it is created with

--- src/test/scala/model/LineTest.scala
+++ src/test/scala/model/LineTest.scala
@@ -1,11 +1,15 @@
 package model
 
 class LineTest extends org.scalatest.flatspec.AnyFlatSpec:
-  
+
   "A Line" should "have length 0 when created with an empty string" in:
     val line = Line("")
     assert(line.length == 0)
-    
+
   it should "have length 0 when created empty" in:
     val line = Line.empty
     assert(line.length == 0)
+
+  it should "have the length of the string it is created with" in:
+    val line = Line("Hello, World!")
+    assert(line.length == 13)
