c99fd30eac3a540cb92b274936d0dcc197d0ccee
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-08-05 18:08:10 +0200

    Line refactor

--- src/main/scala/model/Line.scala
+++ src/main/scala/model/Line.scala
@@ -1,14 +1,14 @@
 package model
 
+trait Line:
+  def length: Int
+  def content: String
+
 object Line:
   
-  trait Line:
-    def length: Int
-    def content: String
-
   private case class LineImpl(s: String) extends Line:
-    def length: Int = s.length
-    def content: String = s
-  
+    override def length: Int = s.length
+    override def content: String = s
+    
   def apply(s: String): Line = LineImpl(s)
   def empty: Line = LineImpl("")
