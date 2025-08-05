6d4e6fd1c2957b89d49d5e9c60ad3f915005afe6
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-08-05 17:21:21 +0200

    Line refactor

--- src/main/scala/model/Line.scala
+++ src/main/scala/model/Line.scala
@@ -2,12 +2,11 @@ package model
 
 object Line:
   opaque type Line = String
-  
+
   def apply(s: String): Line = s
-  
+
   extension (l: Line)
     def length: Int = l.length
     def content: String = l
-
 
   def empty: Line = ""
